3ee6aa00d61ba634dfb07675eb3172b0e4d0dd71
sargam-poudel <sargampoudel100@gmail.com>
2022-01-12 01:54:24 +0545

    added key finder for the window object

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 const Game = () => {
   const [squares, setSquares] = useState([]);
+  const [key, setKey] = useState("");
   useEffect(() => {
     const arr = [];
     for (let i = 0; i < 900; i++) {
@@ -17,7 +18,38 @@ const Game = () => {
     arr[55] = true;
     arr[100] = "apple";
     setSquares(arr);
+    window.addEventListener("keydown", (e) => {
+      handleClick(e.key);
+    });
   }, []);
+
+  const handleClick = (key) => {
+    switch (key) {
+      case "w":
+      case "ArrowUp":
+        console.log("UP");
+        setKey("UP");
+        break;
+      case "ArrowLeft":
+      case "a":
+        console.log("LEFT");
+        setKey("LEFT");
+        break;
+      case "ArrowDown":
+      case "s":
+        console.log("DOWN");
+        setKey("DOWN");
+        break;
+      case "d":
+      case "ArrowRight":
+        console.log("RIGHT");
+        setKey("RIGHT");
+        break;
+      default:
+        console.log(key);
+        break;
+    }
+  };
 
   return (
     <div className="app">
@@ -27,6 +59,7 @@ const Game = () => {
           return <Square snake={item} />;
         })}
       </div>
+      <h1>You pressed {key}</h1>
     </div>
   );
 };
--- src/Square.jsx
+++ src/Square.jsx
@@ -5,12 +5,7 @@ export default function Square({ snake }) {
     <div
       className="square"
       style={{
-        height: "20px",
-        width: "20px",
-        // border: "1px solid black",
         backgroundColor: snake == "apple" ? "red" : snake ? "black" : "pink",
-        margin: 0,
-        padding: 0,
       }}
     ></div>
   );
