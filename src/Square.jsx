7a00f1088f7ec9860047f94bf9b71805bf0bbd0f
sargam-poudel <sargampoudel100@gmail.com>
2022-01-12 00:02:19 +0545

    some great progress

---
+++ src/Square.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Square() {
+  return (
+    <div
+      className="square"
+      style={{
+        height: "20px",
+        width: "20px",
+        border: "1px solid black",
+        backgroundColor: "pink",
+        margin: 0,
+        padding: 0,
+      }}
+    ></div>
+  );
+}
