13e241c5250f225fb53d899fb254a05ba457e742
sargam-poudel <sargampoudel100@gmail.com>
2022-01-13 05:24:47 +0545

    the snake[point] moves and it just works

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ class Board {
     this.boxes = [];
     this.board = document.querySelector(selector);
     this.currHead = {
-      x: 0,
-      y: 0,
+      x: 5,
+      y: 5,
     };
     this.prevHead = {
       x: null,
       y: null,
     };
+    this.dir = [0, 0];
   }
 
   init() {
@@ -23,33 +24,70 @@ class Board {
         if (this.currHead.x === i && this.currHead.y === j) {
           div.style.backgroundColor = "red";
         }
-        div.innerHTML = `${i} ${j}`;
-        div.style.fontSize = "10px";
         this.board.appendChild(div);
         this.boxes[i].push(div);
       }
     }
-    console.log(JSON.stringify(this.boxes));
   }
 
   move() {
-    setInterval(() => {
-      if (this.currHead.x >= this.boxes.length - 1) {
-        this.currHead.x = -1;
-        this.currHead.y += 1;
-        console.log("yes");
-      }
-      if (this.currHead.y >= this.boxes.length - 1) {
-        this.currHead.x = 0;
-        console.log(this.currHead);
-      }
-      this.currHead.x += 1;
+    const loop = setInterval(() => {
+      const [xPos, yPos] = this.dir;
+      this.prevHead.x = this.currHead.x;
+      this.prevHead.y = this.currHead.y;
+      this.currHead.x += xPos;
+      this.currHead.y += yPos;
+      // this.currHead.x += 1;
+      this.update(loop);
+    }, 1000 / 10);
+  }
+
+  update() {
+    try {
+      this.boxes[this.prevHead.x][this.prevHead.y].style.backgroundColor =
+        "pink";
       this.boxes[this.currHead.x][this.currHead.y].style.backgroundColor =
         "red";
-    }, 1000 / 15);
+    } catch (error) {
+      console.log("Game Over bro");
+    }
+  }
+
+  input() {
+    window.addEventListener("keydown", (e) => {
+      const key = e.key;
+      switch (key) {
+        case "w":
+        case "ArrowUp":
+          console.log("UP");
+          this.dir = [-1, 0];
+          this.update();
+          break;
+        case "ArrowLeft":
+        case "a":
+          console.log("LEFT");
+          this.dir = [0, -1];
+          this.update();
+
+          break;
+        case "ArrowDown":
+        case "s":
+          this.dir = [1, 0];
+          console.log("DOWN");
+          this.update();
+          break;
+        case "ArrowRight":
+        case "d":
+          console.log("RIGHT");
+          this.dir = [0, 1];
+          this.update();
+          break;
+      }
+    });
   }
 }
 
-const board = new Board(10, 10, "#board");
+const board = new Board(30, 30, "#board");
 board.init();
-// board.move();
+board.move();
+board.input();
