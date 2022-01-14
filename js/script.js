f0f67cc680f73147a9055f2b13e29bd1199c49ff
sargam-poudel <sargampoudel100@gmail.com>
2022-01-14 22:56:28 +0545

    [📜] `Food` generates randomly and if it overlaps snake it recursively calls itself

--- js/script.js
+++ js/script.js
@@ -26,9 +26,23 @@ class Board {
       x: randRange(0, 29),
       y: randRange(0, 29),
     };
+    if (
+      this.snakeBody.some(
+        (box) => box.x === this.food.x && box.y === this.food.y
+      )
+    ) {
+      console.log("yes");
+      this.getRandomFood();
+    }
   }
 
   init() {
+    this.snakeBody = [
+      {
+        x: this.currHead.x,
+        y: this.currHead.y,
+      },
+    ];
     this.getRandomFood();
     this.score.innerText = `Score: ${this.len}`;
     for (let i = 0; i < this.rows; i++) {
@@ -36,8 +50,11 @@ class Board {
       for (let j = 0; j < this.cols; j++) {
         let div = document.createElement("div");
         div.classList.add("visible");
-        if (this.currHead.x === i && this.currHead.y === j) {
+        if (this.snakeBody[0].x === i && this.snakeBody[0].y === j) {
           div.style.backgroundColor = "red";
+          // div.innerText = `${this.snakeBody[0].x},${this.snakeBody[0].y}`;
+          // div.style.fontSize = "11px";
+          console.log(this.snakeBody);
         }
         if (this.food.x === i && this.food.y === j) {
           div.style.backgroundColor = "blue";
@@ -53,12 +70,15 @@ class Board {
       const [xPos, yPos] = this.dir;
       this.prevHead.x = this.currHead.x;
       this.prevHead.y = this.currHead.y;
-      this.currHead.x += xPos;
       this.currHead.y += yPos;
-      if (this.currHead.x > 29) this.currHead.x = 0;
-      if (this.currHead.x < 0) this.currHead.x = 29;
-      if (this.currHead.y > 29) this.currHead.y = 0;
-      if (this.currHead.y < 0) this.currHead.y = 29;
+      this.currHead.x += xPos;
+      if (this.currHead.x > this.rows - 1) this.currHead.x = 0;
+      if (this.currHead.x < 0) this.currHead.x = this.rows - 1;
+      if (this.currHead.y > this.cols - 1) this.currHead.y = 0;
+      if (this.currHead.y < 0) this.currHead.y = this.cols - 1;
+      if (xPos || yPos) {
+        this.snakeBody.unshift({ x: this.currHead.x, y: this.currHead.y });
+      }
       this.collision();
       this.update(loop);
     }, 1000 / 10);
@@ -72,15 +92,25 @@ class Board {
       this.len++;
       this.score.innerText = `Score: ${this.len}`;
       console.log(this.len);
+      console.log(this.snakeBody);
+    } else {
+      if (this.dir[0] || this.dir[1]) {
+        this.removed = this.snakeBody.pop();
+        this.boxes[this.removed.x][this.removed.y].style.backgroundColor =
+          "pink";
+      }
     }
   }
 
   update() {
     try {
-      this.boxes[this.prevHead.x][this.prevHead.y].style.backgroundColor =
-        "pink";
-      this.boxes[this.currHead.x][this.currHead.y].style.backgroundColor =
-        "red";
+      this.snakeBody.forEach(
+        (box) => (this.boxes[box.x][box.y].style.backgroundColor = "red")
+      );
+      // this.boxes[this.prevHead.x][this.prevHead.y].style.backgroundColor =
+      // "pink";
+      // this.boxes[this.currHead.x][this.currHead.y].style.backgroundColor =
+      // "red";
     } catch (e) {
       console.log(e);
       console.log(this.prevHead);
