4d578f1cf083bc1f293552f7a7b9a37c9af3eaa6
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2022-10-13 23:37:04 -0500

    Fix capitalization & add more themes

--- src/scripts/themes.js
+++ src/scripts/themes.js
@@ -1,17 +1,38 @@
 export const themes = {
-  dracula: {
+  Dracula: {
     bg: "#282a36",
     food: "#6272a4",
     snake: "#f8f8f2",
   },
-  pink: {
+  Pink: {
     bg: "pink",
     food: "red",
     snake: "black",
   },
-  classic: {
+  Classic: {
     bg: "#33cc33",
     food: "#8c8c8c",
     snake: "black",
   },
+  Beach: {
+    bg: "#FFEBAD",
+    food: "#F9F9F9",
+    snake: "#C3F8FF",
+  },
+  "Black & White": {
+    bg: "#212121",
+    food: "#808080",
+    snake: "#F5F5F5",
+  },
+  Royal: {
+    bg: "#905E96",
+    food: "#E80F88",
+    snake: "#FFD372",
+  },
+  Hacktoberfest: {
+    bg: "#151515",
+    food: "#FF5B00",
+    snake: "#FFEE63",
+  }
 };
+
